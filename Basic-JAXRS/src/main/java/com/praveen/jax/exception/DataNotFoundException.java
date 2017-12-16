a3e3ffadc890b4e98262d2fff0358b5ef168d4ed
prambi <prambi@DIN38000018.corp.capgemini.com>
2017-12-16 17:00:37 +0530

    Second commit

--- Basic-JAXRS/src/main/java/com/praveen/jax/exception/DataNotFoundException.java
+++ Basic-JAXRS/src/main/java/com/praveen/jax/exception/DataNotFoundException.java
@@ -5,11 +5,17 @@ public class DataNotFoundException extends RuntimeException {
 	/**
 	 * 
 	 */
+	
+	// Git hub   commit
+	
+	//asdfuhafjafdfdfjn
 	private static final long serialVersionUID = -2728754176362296463L;
 	
 	public DataNotFoundException(String message)
 	{
 		super(message);
+		
+		System.out.println("message is ----" + message);
 		
 	}
 
